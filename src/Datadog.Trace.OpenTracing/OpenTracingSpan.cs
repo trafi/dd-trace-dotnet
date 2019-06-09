1256be648c86db01fc31d08b402d09916fa41da1
dsedleckas <dominykas.sedleckas@gmail.com>
2019-06-09 15:09:21 +0300

    Log debug on span creation

--- src/Datadog.Trace.OpenTracing/OpenTracingSpan.cs
+++ src/Datadog.Trace.OpenTracing/OpenTracingSpan.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using Datadog.Trace.Logging;
 using OpenTracing;
 using OpenTracing.Tag;
 
@@ -8,8 +9,11 @@ namespace Datadog.Trace.OpenTracing
 {
     internal class OpenTracingSpan : ISpan
     {
+        private static readonly ILog _log = LogProvider.For<OpenTracingSpan>();
+
         internal OpenTracingSpan(Span span, IEnumerable<KeyValuePair<string, string>> baggage = null)
         {
+            _log.Debug($"Creating OTSpan: {span.OperationName} {span.ResourceName} {span.ServiceName}");
             Span = span;
             Context = new OpenTracingSpanContext(span.Context, baggage);
         }
